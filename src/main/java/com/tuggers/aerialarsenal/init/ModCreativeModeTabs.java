89e4ce09b3545c54efa42831a2b03bcbbb928aea
Jack <jack.carter334@gmail.com>
2023-11-08 13:02:09 +0100

    Initial commit

---
+++ src/main/java/com/tuggers/aerialarsenal/init/ModCreativeModeTabs.java
@@ -0,0 +1,29 @@
+package com.tuggers.aerialarsenal.init;
+
+import com.tuggers.aerialarsenal.AerialArsenal;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModCreativeModeTabs {
+
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, AerialArsenal.MODID);
+
+    public static final RegistryObject<CreativeModeTab> AERIAL_ARSENAL_TAB = CREATIVE_MODE_TABS.register("aerial_arsenal_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.RAW_SAPPHIRE.get()))
+                    .title(Component.translatable("creativetab.aerial_arsenal_tab"))
+                    .displayItems((pParameters, pOutput) -> {
+                        pOutput.accept(ModItems.RAW_SAPPHIRE.get());
+                    })
+                    .build());
+
+    public static void register(IEventBus eventBus) {
+        CREATIVE_MODE_TABS.register(eventBus);
+    }
+
+}
---
+++ src/main/java/com/tuggers/aerialarsenal/init/ModItems.java
@@ -0,0 +1,24 @@
+package com.tuggers.aerialarsenal.init;
+
+import com.tuggers.aerialarsenal.AerialArsenal;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, AerialArsenal.MODID);
+
+    public static final RegistryObject<Item> RAW_SAPPHIRE = ITEMS.register("raw_sapphire",
+            () -> new Item(new Item.Properties()));
+
+
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+
+}
