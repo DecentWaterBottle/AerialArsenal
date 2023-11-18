4f28a3f6dc5ac0ec4e71e7f51c3bb9b35dbc00e4
Jack <jack.carter334@gmail.com>
2023-11-18 14:48:46 +0100

    Create knife pickup when knife entity hits another entity.

--- src/main/java/com/tuggers/aerialarsenal/entity/KnifeEntity.java
+++ src/main/java/com/tuggers/aerialarsenal/entity/KnifeEntity.java
@@ -6,6 +6,7 @@ import net.minecraft.network.protocol.Packet;
 import net.minecraft.network.protocol.game.ClientGamePacketListener;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.AbstractArrow;
 import net.minecraft.world.item.Item;
@@ -13,6 +14,8 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Tier;
 import net.minecraft.world.item.Tiers;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.EntityHitResult;
+import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.network.NetworkHooks;
 
@@ -26,8 +29,9 @@ public class KnifeEntity extends AbstractArrow {
 
     }
 
-    protected KnifeEntity(EntityType<? extends AbstractArrow> pEntityType, double pX, double pY, double pZ, Level pLevel) {
+    protected KnifeEntity(EntityType<? extends AbstractArrow> pEntityType, double pX, double pY, double pZ, Level pLevel, Tiers tier) {
         super(pEntityType, pX, pY, pZ, pLevel);
+        this.entityTier = tier;
     }
 
     public KnifeEntity(EntityType<KnifeEntity> entity, LivingEntity pShooter, Level pLevel) {
@@ -68,9 +72,34 @@ public class KnifeEntity extends AbstractArrow {
     }
 
     @Override
-    protected boolean tryPickup(Player pPlayer) {
-        return super.tryPickup(pPlayer) || this.isNoPhysics() && this.ownedBy(pPlayer) && pPlayer.getInventory().add(this.getPickupItem());
+    protected void onHitEntity(EntityHitResult pResult) {
+//        ItemEntity droppedItem = new ItemEntity(this.level(), this.getX(), this.getY(), this.getZ(), new ItemStack(ModItems.STONE_KNIFE.get()));
+//        this.level().addFreshEntity(droppedItem);
+//        LivingEntity shooter = this.getOwner() instanceof LivingEntity ? (LivingEntity) this.getOwner() : null;
+
+        super.onHitEntity(pResult);
+
+        if (this.pickup == AbstractArrow.Pickup.ALLOWED) {
+            if (!this.level().isClientSide) {
+
+                KnifeEntity knifeEntity = switch (entityTier) {
+                    case WOOD ->                new KnifeEntity(ModEntities.WOODEN_KNIFE_ENTITY.get(), this.getX(), this.getY(), this.getZ(), this.level(), entityTier);
+                    case STONE ->               new KnifeEntity(ModEntities.STONE_KNIFE_ENTITY.get(), this.getX(), this.getY(), this.getZ(), this.level(), entityTier);
+                    default ->                  new KnifeEntity(ModEntities.STONE_KNIFE_ENTITY.get(), this.getX(), this.getY(), this.getZ(), this.level(), entityTier);
+                };
+                knifeEntity.pickup = Pickup.ALLOWED;
+
+                Vec3 motion = knifeEntity.getDeltaMovement();
+                double angle = Math.atan2(-motion.x, motion.z);
+                double degrees = Math.toDegrees(angle);
+
+                knifeEntity.setYRot((float)degrees + 90.0F);
+                this.level().addFreshEntity(knifeEntity);
+            }
+        }
+
     }
+
 
     public Packet<ClientGamePacketListener> getAddEntityPacket() {
         return NetworkHooks.getEntitySpawningPacket(this);
